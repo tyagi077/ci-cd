72801a93bf63e89361303216953083362aaeb46e
Mayank Tyagi <tyagimayank0501@gmail.com>
2025-04-06 23:22:48 +0530

    added full path

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 cd ci-cd
+git pull origin main
 /home/ubuntu/.nvm/versions/node/v22.14.0/bin/npm install
 /home/ubuntu/.nvm/versions/node/v22.14.0/bin/npm run build
 /home/ubuntu/.nvm/versions/node/v22.14.0/bin/npm run start
